b54e7014f87afbd4dec79c0dee59abd337b664ce
Sudesh Sharma <Sudesh.Sharma@visertechnosys.local>
2019-06-13 17:43:18 +0530

    updated root file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Testing github pages</h1>
+        <h1>Testing github pages with cloned repo from github version control system</h1>
       </header>
     </div>
   );
