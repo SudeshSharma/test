55d5400965c29aa4618cce2bcf771a3de3f1c20e
Sudesh Sharma <43905979+SudeshSharma@users.noreply.github.com>
2019-06-17 17:23:52 +0530

    Create Aboutme.js

---
+++ Aboutme.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Aboutme extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>This is author about me page.Please React accordingly.</h1>
+      </div>
+    )
+  }
+}
