14430ef798dcb50a6626637044306caffb6af4be
sudeshsharma <sudesh18sharma@gmail.com>
2019-06-17 17:48:00 +0530

    Updated About Branch

--- Aboutme.js
+++ Aboutme.js
@@ -4,7 +4,7 @@ class Aboutme extends React.Component{
   render(){
     return(
       <div>
-        <h1>This is author about me page.Please React accordingly.</h1>
+        <h1>This page edited by sudesh sharma.</h1>
       </div>
     )
   }
